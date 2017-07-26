1c494993cff16e9b117d60d28fdcfc0f7760fdd1
DoTruong <tmoz113011@gmail.com>
2017-07-26 13:13:21 +0700

    Build CRUD Category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,49 @@
+class CategoriesController < ApplicationController
+  before_action :find_category, except: [:index, :create]
+
+  def index
+    @category = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = "success"
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def show;  end
+
+  def edit; end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = t ".success"
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:success] = t ".success", category: @category.name
+    redirect_to categories_url
+  end
+
+  private
+  def find_category
+    @category = Category.find_by id: params[:id]
+  end
+
+  def category_params
+    params.require(:category).permit :name
+  end
+end
