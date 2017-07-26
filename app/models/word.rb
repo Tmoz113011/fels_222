11a269419bf2045d8fe8ea16abea4f42ae049577
TranTuanManh <manhit1706@gmail.com>
2017-07-26 09:44:10 +0700

    Fix end file word.rb va thieu index trong bang relationship

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,3 +4,4 @@ class Word < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :results, dependent: :destroy
   has_many :lessons, through: :results
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725091322) do
+ActiveRecord::Schema.define(version: 20170726024932) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "activity_type"
@@ -51,6 +51,9 @@ ActiveRecord::Schema.define(version: 20170725091322) do
     t.integer  "followed_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "results", force: :cascade do |t|
