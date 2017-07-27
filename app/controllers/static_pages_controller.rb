01f0e97a553beca15ce8005f7cb67f6cb8049aa4
DoTruong <tmoz113011@gmail.com>
2017-07-27 16:13:41 +0700

    Build Login sign up log out

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,4 @@
 class StaticPagesController < ApplicationController
+  before_action :logged_in_user, :admin_user
   def home; end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, except: [:new, :create, :show]
   before_action :find_user, except: [:new, :index, :create]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate page: params[:page]
